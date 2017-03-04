29d684ec61a147aa77fafd3129473e7fc3ba05f3
mxx <mengxxself>
2017-03-04 16:57:33 +0800

    tabs

--- index.js
+++ index.js
@@ -1,14 +1,7 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import {Router, Route, IndexRoute, hashHistory} from 'react-router';
 
-class App extends Component {
-    render() {
-        return (
-            <div>app</div>
-        )
-    }
-}
+import {Tabs} from './components'
 
 ReactDOM.render(
-    <App/>, document.querySelector('#app'));
+    <Tabs/>, document.querySelector('#app'));
