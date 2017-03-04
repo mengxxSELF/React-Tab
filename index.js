34a0416f3083948618d56c6ea1c490edd8fabc0c
webmaxxSELF <webmxxself@163.com>
2017-03-04 17:07:21 +0800

    tans

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 
-import {Tabs} from './components'
+import Tabs from './components'
 
 ReactDOM.render(
     <Tabs/>, document.querySelector('#app'));
